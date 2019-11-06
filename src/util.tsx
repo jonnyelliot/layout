77bf3f4c375a2b281495d9e3562d0dc548d7f3b9
Stuart Hendren <stuart@committed.io>
2019-11-06 10:00:49 +0000

    :memo: Improve docs

--- src/util.tsx
+++ src/util.tsx
@@ -28,45 +28,54 @@ export interface LayoutConfig {
   /**
    * clipped, moves the header over the top of the navigation drawer
    * unclipped makes navigation full height
+   * @default false
    */
   clipped: boolean | ScreenProps<boolean>
   /**
-  * Can the navigation be collapsed to a smalled form
-  */
+   * Can the navigation be collapsed to a smaller form
+   * @default true
+   */
   collapsible: boolean | ScreenProps<boolean>
   /**
    * Width of the collapsed navigation
+   * @default 64
    */
   collapsedWidth: number | ScreenProps<number>
   /**
    * Footer to adjust the size to fit when nav expanded,
    * set false to keep the same width and overflow the screen.
+   * @default true
    */
   footerShrink: boolean | ScreenProps<boolean>
   /**
    * Which side of the screen to show the nav panel
+   * @default left
    */
   navAnchor: Orientation | ScreenProps<Orientation>
   /**
-  * Navigation variant:
-  * - permanent: stays all the time
-  * - persistent: remains open but can be hidden with button
-  * - temporary: hides on click away (and selection)
-  */
+   * Navigation variant:
+   * - permanent: stays all the time
+   * - persistent: remains open but can be hidden with button
+   * - temporary: hides on click away (and selection)
+   * @default permanent
+   */
   navVariant: Variant | ScreenProps<Variant>
   /**
-  * Width of the navigation drawer
-  */
+   * Width of the navigation drawer
+   * @default 256
+   */
   navWidth: number | ScreenProps<number>
   /**
-   * Popsition applied to the AppBar header
+   * Position applied to the AppBar header
    * one of 'static', 'relative', 'sticky', 'fixed', 'absolute'
    * See https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Positioning
+   * @default relative
    */
   headerPosition: Position | ScreenProps<Position>
   /**
    * Both header and content adjust the size to fit when nav expanded,
    * set false to keep the same width and overflow the screen.
+   * @default true
    */
   squeezed: boolean | ScreenProps<boolean>
 }
